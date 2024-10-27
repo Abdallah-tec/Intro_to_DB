1ba89e8f238055083e24efe6cc41416f5b426e72
Abdallah Sabry <sabryabdallah37@gmail.com>
2024-10-28 00:24:50 +0300

    replaced errors with Error

--- MySQLServer.py
+++ MySQLServer.py
@@ -10,11 +10,11 @@ try:
     try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
-    except mysql.connector.errors as er:
+    except mysql.connector.Error as er:
         print(f"Failed creating database: {er}")
     cursor.close()
     mydb.close()
 
-except mysql.connector.errors as er:
+except mysql.connector.Error as er:
     print(f"failed with{er}")
     
