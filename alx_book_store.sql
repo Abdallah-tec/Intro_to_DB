fcf04d02b64a968b33330e4dccf04b332f2bdef3
Abdallah Sabry <sabryabdallah37@gmail.com>
2024-10-28 00:07:14 +0300

    updated

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,10 +1,12 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 CREATE TABLE Books(
 book_id INT PRIMARY KEY,
 title VARCHAR(130),
 author_id INT,
-FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+FOREIGN KEY(author_id) REFERENCES Authors(author_id),
+price DOUBLE,
+publication_date DATE
 );
 CREATE TABLE Authors(
 author_id INT PRIMARY KEY,
