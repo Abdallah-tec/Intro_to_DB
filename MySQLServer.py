9dfb0edbc8138fd6475e627314283dbc5df4052b
Abdallah Sabry <sabryabdallah37@gmail.com>
2024-10-28 00:22:41 +0300

    v1

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,2 +1,20 @@
-import _mysql_connector
-mydb = 
+import mysql.connector
+
+try:
+    mydb = mysql.connector.connect(
+        host ='localhost',
+        user = 'root',
+        password ='Progamer12345'
+    )
+    cursor =mydb.cursor()
+    try:
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+    except mysql.connector.errors as er:
+        print(f"Failed creating database: {er}")
+    cursor.close()
+    mydb.close()
+
+except mysql.connector.errors as er:
+    print(f"failed with{er}")
+    
